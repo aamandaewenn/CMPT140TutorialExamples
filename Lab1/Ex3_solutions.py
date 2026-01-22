1013c198c6c3654cd0865aea48e8b789dc393cf7
Amanda <are390@usask.ca>
2026-01-22 16:11:26 -0600

    in class solutions to exercises

---
+++ Lab1/Ex3_solutions.py
@@ -0,0 +1,18 @@
+from p5 import *
+
+def setupt():
+    size(200, 200)
+
+def draw():
+    # Draw a rectangle
+    rect(10, 10, 75, 200)
+
+    # add a coordinate
+    point(30, 40)
+
+    # add some text
+    text("Hello World!", 200, 100)
+
+    
+    
+run()
